f308ca3cf65b0099c23c7d5df3d7c2fcd957bdf5
jllahi <1439075+jllahi@users.noreply.github.com>
2024-06-15 17:08:49 +0200

    add astro-relative-links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // import tailwind from '@astrojs/tailwind'
-import metaTags from 'astro-meta-tags'
-import { defineConfig } from 'astro/config'
+import metaTags from 'astro-meta-tags';
+import { defineConfig } from 'astro/config';
+
+import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [metaTags()],
-})
+  integrations: [metaTags(), relativeLinks()]
+});
