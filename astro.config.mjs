b015a9d0025f49385b141a4a0376b1122bb4db17
jllahi <1439075+jllahi@users.noreply.github.com>
2024-06-15 17:24:19 +0200

    add trailingSlash always

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import relativeLinks from 'astro-relative-links'
 export default defineConfig({
   site: 'https://jllahi.github.io',
   base: '/github-actions-astro',
+  trailingSlash: 'always',
   integrations: [metaTags(), relativeLinks()],
 })
