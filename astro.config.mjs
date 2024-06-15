9a45b65bc3439e341817b9c3aac669705127416b
jllahi <1439075+jllahi@users.noreply.github.com>
2024-06-15 17:20:50 +0200

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // import tailwind from '@astrojs/tailwind'
-import metaTags from 'astro-meta-tags';
-import { defineConfig } from 'astro/config';
+import metaTags from 'astro-meta-tags'
+import { defineConfig } from 'astro/config'
 
-import relativeLinks from "astro-relative-links";
+import relativeLinks from 'astro-relative-links'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [metaTags(), relativeLinks()]
-});
+  site: 'https://jllahi.github.io/github-actions-astro/',
+  integrations: [metaTags(), relativeLinks()],
+})
